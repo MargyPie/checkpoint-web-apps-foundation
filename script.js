8a912e56fa823313fc31d9bf33107a2f3dab4058
Martina Recke <tinyty@web.de>
2022-05-09 13:16:21 +0200

    feat: adds lower-case validation

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const userPw = document.querySelector("#user-pw");
 const confirmationPw = document.querySelector("#confirmation-pw");
 
 const checkEquality = document.querySelector("#check-equality");
+const checkLower = document.querySelector("#check-lower");
+const checkUpper = document.querySelector("#check-upper");
+const checkNum = document.querySelector("#check-num");
+const checkLength = document.querySelector("#check-length");
 
 const span = document.querySelector("span");
 
@@ -21,20 +25,33 @@ function passwordVisibility() {
 }
 
 function checkRequirements() {
+  const requirementLowerCase = new RegExp("^(?=.*[a-z])");
+  const requirementUpperCase = new RegExp("^(?=.*[A-Z])");
+  const requirementNumber = new RegExp("^(?=.*[0-9])");
+  const requirementLength = new RegExp("^(?=.{10,})");
+
   /***************** Check: Equality ****************/
   if (confirmationPw.value !== userPw.value) {
     console.log("Passwords do not match!");
+    checkEquality.textContent = "❌";
+    checkLower.textContent = "❌";
   } else {
     console.log("pw match");
     checkEquality.textContent = "✅";
+
+    /***************** Check: Lower-Case ****************/
+    if (!requirementLowerCase.test(userPw)) {
+      checkLower.textContent = "❌";
+    } else {
+      checkLower.textContent = "✅";
+    }
+    /***************** Check: Upper-Case ****************/
+    /***************** Check: Number ****************/
+    /***************** Check: Length ****************/
+    // if (inputBox.length > 3) {
+    //   span.textContent = " ✅";
+    // }
   }
-  /***************** Check: Lower-Case ****************/
-  /***************** Check: Upper-Case ****************/
-  /***************** Check: Number ****************/
-  /***************** Check: Length ****************/
-  // if (inputBox.length > 3) {
-  //   span.textContent = " ✅";
-  // }
 }
 
 btn.addEventListener("click", passwordVisibility);
