280ab2522d73c5ba2400d144d3f37abc10ec8faa
Martina Recke <tinyty@web.de>
2022-05-09 10:49:17 +0200

    feat: adds equality check

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const btn = document.querySelector("button");
+const inputBox = document.querySelector("input");
+const userPw = document.querySelector("#user-pw");
+const confirmationPw = document.querySelector("#confirmation-pw");
+
+const checkEquality = document.querySelector("#check-equality");
+
+const span = document.querySelector("span");
 
 /***************** Button: Toggle Password Visibility **************/
 function passwordVisibility() {
-  const inputBox = document.querySelector("input");
-
   if (btn.classList.contains("hiddenPw")) {
     btn.classList.toggle("hiddenPw");
     inputBox.setAttribute("type", "text");
@@ -13,12 +18,24 @@ function passwordVisibility() {
     inputBox.setAttribute("type", "password");
     btn.textContent = "Show Password";
   }
+}
 
+function checkRequirements() {
   /***************** Check: Equality ****************/
+  if (confirmationPw.value !== userPw.value) {
+    console.log("Passwords do not match!");
+  } else {
+    console.log("pw match");
+    checkEquality.textContent = "✅";
+  }
   /***************** Check: Lower-Case ****************/
   /***************** Check: Upper-Case ****************/
   /***************** Check: Number ****************/
   /***************** Check: Length ****************/
+  // if (inputBox.length > 3) {
+  //   span.textContent = " ✅";
+  // }
 }
 
 btn.addEventListener("click", passwordVisibility);
+confirmationPw.addEventListener("input", checkRequirements);
