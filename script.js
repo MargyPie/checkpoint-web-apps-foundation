dd4701e1e1a718322ebf314c76a5bdf712cfaf8b
Martina Recke <tinyty@web.de>
2022-05-09 21:12:59 +0200

    feat: implements upper case, number and length validations

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const btn = document.querySelector("button");
-const inputBox = document.querySelector("input");
+const inputBox = document.querySelector("#user-pw");
 const userPw = document.querySelector("#user-pw");
 const confirmationPw = document.querySelector("#confirmation-pw");
 
@@ -9,17 +9,17 @@ const checkUpper = document.querySelector("#check-upper");
 const checkNum = document.querySelector("#check-num");
 const checkLength = document.querySelector("#check-length");
 
-const span = document.querySelector("span");
-
 /***************** Button: Toggle Password Visibility **************/
 function passwordVisibility() {
   if (btn.classList.contains("hiddenPw")) {
     btn.classList.toggle("hiddenPw");
-    inputBox.setAttribute("type", "text");
+    userPw.setAttribute("type", "text");
+    confirmationPw.setAttribute("type", "text");
     btn.textContent = "Hide Password";
   } else {
     btn.classList.toggle("hiddenPw");
-    inputBox.setAttribute("type", "password");
+    userPw.setAttribute("type", "password");
+    confirmationPw.setAttribute("type", "password");
     btn.textContent = "Show Password";
   }
 }
@@ -35,24 +35,43 @@ function checkRequirements() {
     console.log("Passwords do not match!");
     checkEquality.textContent = "❌";
     checkLower.textContent = "❌";
+    checkUpper.textContent = "❌";
+    checkNum.textContent = "❌";
+    checkLength.textContent = "❌";
   } else {
     console.log("pw match");
     checkEquality.textContent = "✅";
 
     /***************** Check: Lower-Case ****************/
-    if (!requirementLowerCase.test(userPw)) {
+    if (!requirementLowerCase.test(userPw.value)) {
       checkLower.textContent = "❌";
     } else {
       checkLower.textContent = "✅";
     }
+
     /***************** Check: Upper-Case ****************/
+    if (!requirementUpperCase.test(userPw.value)) {
+      checkUpper.textContent = "❌";
+    } else {
+      checkUpper.textContent = "✅";
+    }
+
     /***************** Check: Number ****************/
+    if (!requirementNumber.test(userPw.value)) {
+      checkNum.textContent = "❌";
+    } else {
+      checkNum.textContent = "✅";
+    }
+
     /***************** Check: Length ****************/
-    // if (inputBox.length > 3) {
-    //   span.textContent = " ✅";
-    // }
+    if (!requirementLength.test(userPw.value)) {
+      checkLength.textContent = "❌";
+    } else {
+      checkLength.textContent = "✅";
+    }
   }
 }
 
 btn.addEventListener("click", passwordVisibility);
 confirmationPw.addEventListener("input", checkRequirements);
+userPw.addEventListener("input", checkRequirements);
